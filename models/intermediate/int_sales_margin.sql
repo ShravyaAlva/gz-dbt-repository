606fb9c6885194fdad3b7e32e0dde8d016b37d25
shravyaalva1057@gmail.com <shravyaalva1057@gmail.com>
2025-10-30 16:37:05 +0000

    changed to purchse_price

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ SELECT
     ,orders_id
     ,revenue
     ,quantity
-    ,purchase_price
-    ,ROUND(s.quantity*p.purchase_price, 2) as purchase_cost
-    ,ROUND(s.revenue-s.quantity*p.purchase_price, 2) as margin
+    ,purchse_price
+    ,ROUND(s.quantity*p.purchse_price, 2) as purchase_cost
+    ,ROUND(s.revenue-s.quantity*p.purchse_price, 2) as margin
 FROM {{ref('stg_gz_raw_data__raw_gz_sales')}} s
 LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_product")}} p
     USING(products_id)

49926bb94dde0490cd210eb627ea4cd07f26bbec
shravyaalva1057@gmail.com <shravyaalva1057@gmail.com>
2025-10-30 11:57:43 +0000

    products id misatke

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
 SELECT
-product_id
-,date_date
-,orders_id
-,revenue
-,quantity
-,purchase_cost
-,ROUND(s.quantity*p.purchase_price,2) as purchase_cost
-,ROUND(s.revenue-s.quantity*p.purchase_price,2) as margin
-FROM {{ref("stg_gz_raw_data__raw_gz_sales")}} s
+    products_id
+    ,date_date
+    ,orders_id
+    ,revenue
+    ,quantity
+    ,purchase_cost
+    ,ROUND(s.quantity*p.purchase_price, 2) as purchase_cost
+    ,ROUND(s.revenue-s.quantity*p.purchase_price, 2) as margin
+FROM {{ref('stg_gz_raw_data__raw_gz_sales')}} s
 LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_product")}} p
-USING(product_id) 
+    USING(products_id) 

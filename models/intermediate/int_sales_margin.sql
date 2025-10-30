47f62f6617602f2410ad8ddafc12663f1d17595d
shravyaalva1057@gmail.com <shravyaalva1057@gmail.com>
2025-10-30 11:48:37 +0000

    clean, join and created margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -9,4 +9,4 @@ product_id
 ,ROUND(s.revenue-s.quantity*p.purchase_price,2) as margin
 FROM {{ref("stg_gz_raw_data__raw_gz_sales")}} s
 LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_product")}} p
-USING(product_id)
+USING(product_id) 
